827c04b2ca43568578dd455758b2b28ea75e0f24
Andreas Hedman Bergsvik <bergsvik@gmail.com>
2020-03-31 15:01:52 +0200

    add localstorage and save loaded file

--- css_theme_loader.js
+++ css_theme_loader.js
@@ -1,5 +1,23 @@
 window.addEventListener('load', (event) => {
 
+
+const stylesheets = [
+  { name: "1177", path: "https://www.1177.se/build/1177/stylesheets/app.css" },
+  { name: "umo", path: "https://www.umo.se/build/umo/stylesheets/app.css" }, 
+];
+
+function getCurrentSite() {
+  if(!window.localStorage) return false;  
+  return window.localStorage.getItem('inera_jsfiddle_current_site');
+}
+
+function setCurrentSite(site) {
+  if(!window.localStorage) return false;  
+  window.localStorage.setItem('inera_jsfiddle_current_site', site);
+}
+
+const CURRENT = getCurrentSite();
+
 function loadCSS(path) {
   const style   = document.createElement('link');
   style.rel = 'stylesheet';
@@ -11,41 +29,102 @@ function loadCSS(path) {
 
 function clickHandler(e) {
   e.preventDefault();
+  document.getElementById('cssLinkWrapper').classList.remove('heavy');
+  const links = document.querySelectorAll('.jsfiddle_css_theme');
+  links.forEach(el => el.classList.remove('active'));
+  
   const el = document.getElementById('dynamic-css');
   if(el) el.remove();
+  e.target.classList.add('active');
+  setCurrentSite(e.target.innerText);
   loadCSS(e.target.href);
 }
 
 	
 const wrapper = document.createElement("div");
 wrapper.id = "cssLinkWrapper";
-wrapper.style.position = "fixed";
-wrapper.style.top = "0";
-wrapper.style.right = "0";
+if(!CURRENT) {
+  wrapper.classList.add('heavy');
+} else {
+  loadCSS(getPathFromName(CURRENT));
+}
 document.body.appendChild(wrapper);
 
+// add some css! 
+const css = document.createElement('style');
+
+css.innerHTML = `
+  body {
+    padding-top: 40px !important;
+  }
+  #cssLinkWrapper {
+    box-sizing: content-box;
+    position: fixed; 
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: auto;
+    height: 40px;
+    background-color: #111;
+    border-bottom: 2px solid #111;
+    line-height: 40px;
+    font-size: 14px;
+  }
+  #cssLinkWrapper.heavy {
+    background-color: hotpink;
+    background-color: rgba(0, 0, 0, 0.7);
+    top: 100px;
+    padding: 20px;
+    left: 50%;
+    margin-left: -100px;
+    right: auto;
+    bottom: auto;
+    width: 200px;
+    font-size: 32px;
+    border: 3px solid #111;
+    border-radius: 20px;
+    text-align: center;
+  }
+	.jsfiddle_css_theme {
+    color: white;
+    display: inline-block;
+    padding-left: 10px;
+    padding-right: 10px;
+    font-size: inherit;
+  }
+  .jsfiddle_css_theme:focus,
+	.jsfiddle_css_theme:hover {
+  	color: hotpink;
+  }
+  .jsfiddle_css_theme.active {
+  	color: #111;
+    background-color: #f8f8f8;
+  }
+`;
+document.body.appendChild(css);
 
 function createLink(item) {
+	const current = getCurrentSite();
   const a = document.createElement('a');
   const w = document.getElementById("cssLinkWrapper");	
   a.href = item.path;
+  a.className = 'jsfiddle_css_theme';
+  a.id = item.name;
   a.innerHTML = item.name;
-  a.style.display = "inline-block";
-  a.style.padding = "3px";
-  a.style.fontSize = "12px";
   a.addEventListener("click", clickHandler);
+  if(item.name === current) {
+  	a.classList.add('active');
+  }
   if(w) w.appendChild(a);
   else document.body.appendChild(a);
 }
 
 	
-const stylesheets = [
-	{ name: "1177", path: "https://www.1177.se/build/1177/stylesheets/app.css" },
-  { name: "umo", path: "https://www.umo.se/build/umo/stylesheets/app.css" }, 
-];
+
+function getPathFromName(name) {
+  return stylesheets.find(i => i.name === name).path;
+}
 	
 stylesheets.forEach(createLink);
-
-loadCSS(stylesheets[0].path);
 
 });
